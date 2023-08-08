dfcac0f15e3b361ecf5829cca7db67040f986637
Matias Farah <maticole1980@gmail.com>
2023-08-07 22:55:11 -0300

    js tercer entrega

--- codigo.js
+++ codigo.js
@@ -1,71 +1,88 @@
 const ANUAL= 365
 
+
 const OPCION1 = {
+
 
     montoMaximo: 1000000,
     plazo: 30,
     tasa: 0.90
 
+
 }
 const OPCION2 = {
+
 
     montoMaximo: 2000000,
     plazo: 60,
     tasa: 0.95
 
+
 }
 const OPCION3 = {
+
 
     montoMaximo: 3000000,
     plazo: 90,
     tasa: 0.99
 
+
 }
+
 
 const OPCIONES = [OPCION1, OPCION2, OPCION3];
 
+
 function calcularOpcion (montoDeInversion) {
-    // for (let index = 0; index < OPCIONES.length; index++) {
-    //     const opcion = OPCIONES[index];
-    //     if (opcion.montoMaximo >=montoDeInversion) {
-    //         return opcion;
-    //     }
-        
-    // }
     return OPCIONES.find ((opcion) => opcion.montoMaximo >=montoDeInversion ) 
     
 
-}
 
+}
 
 
 function calculoFinal (montoDeInversion, opcion){
     return (((montoDeInversion * opcion.tasa) / ANUAL)* opcion.plazo);
     
 
-}
 
-const nombreUsuario = prompt ("Ingrese su nombre");
-
-console.log (nombreUsuario);
-
-alert("hola " + nombreUsuario + "!");
-
-let montoDeInversion = prompt ("Por Favor, ingrese el monto en Pesos Argentinos que desea calcular");
-
-while (montoDeInversion <500000){
-    alert("Ese monto es muy bajo, por favor, ingresa monto desde $500000");
-    montoDeInversion = prompt ("Por Favor, ingrese el monto en Pesos Argentinos que desea calcular");
 }
 
 
-alert("El monto es valido, puedes continuar");
+
+function calcular (event){
+    event.preventDefault();        
 
 
-const opcionElegida = calcularOpcion (montoDeInversion)
+    const nombreUsuario = document.getElementById("nombre").value;
+    
+    const guardaNombre = {
+        nombre: nombreUsuario,
+    }
+
+
+    const usuarioJSON = JSON.stringify(guardaNombre);
+    localStorage.setItem("nombreUsuario", usuarioJSON);
+
+
+    const nombreUsuarioEnLocalStorage = localStorage.getItem("nombreUsuario");
+    const usuarioObjeto = JSON.parse(nombreUsuarioEnLocalStorage);
+    const nombreLS = document.getElementById("nombre").value;
 
 
 
-let resultado = calculoFinal (montoDeInversion,opcionElegida);
-console.log (resultado);
-    alert (nombreUsuario + " El interes por " + opcionElegida.plazo + " dias es de $" + resultado );
+    
+
+
+    const montoDeInversion = document.getElementById("montoDeInversion").value;
+
+
+    const opcionElegida = calcularOpcion (montoDeInversion)
+
+
+    const resultado = calculoFinal (montoDeInversion,opcionElegida);
+    const textoResultado = document.getElementById("textoResultado");
+    textoResultado.textContent = nombreUsuario + "  El interes por " + opcionElegida.plazo + "   dias es de   " + resultado;
+}
+const boton= document.getElementById("boton");
+boton.addEventListener("click", calcular);
