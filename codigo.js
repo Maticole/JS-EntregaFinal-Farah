5bd7f5268612245343e7f637a9b36a16f1fcd95c
Maticole <133237770+Maticole@users.noreply.github.com>
2023-08-24 18:49:27 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -70,7 +70,6 @@ function calcular (event){
     const nombreLS = document.getElementById("nombre").value;
 
 
-
     
 
 
@@ -86,3 +85,57 @@ function calcular (event){
 }
 const boton= document.getElementById("boton");
 boton.addEventListener("click", calcular);
+
+
+    
+const sweetAlert = document.querySelector("#sweetAlert");
+
+sweetAlert.addEventListener("click", (event) => {
+    event.preventDefault();
+    const edadUsuario = document.getElementById("edad").value;
+const guardaEdad = {
+    edad: edadUsuario,
+}
+
+console.log (edadUsuario >= 18 ? "podés ingresar" : "No podés Ingresar, sos menor");
+    
+    
+    if (edadUsuario < 18){
+
+        
+    Swal.fire({
+        title: 'Eres Menor!',
+        text: "No puedes Continuar",
+        icon: 'error',
+        confirmButtonText: 'Aceptar',
+        
+        
+    })
+
+    }
+})
+
+async function mostrarClima() {
+    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Argentina'
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5f92cb857amsh5c8c05d9020f306p1249c2jsn5e3225a6cb45',
+            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const clima= result.current.temp_c;
+        const climatologia = document.getElementById("climatologia");
+        climatologia.textContent = clima + "  Grados"
+
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+mostrarClima();
